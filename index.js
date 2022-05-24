bd22b8c02d45963d08a5979878deb4c68404b056
Shuvapandit <chakrabortyshuva216@gmail.com>
2022-05-24 21:24:37 +0600

    manufacturer-website-server-side initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello From All!n1store')
+})
+
+app.listen(port, () => {
+  console.log(`All!n1store' App listening on port ${port}`)
+})
